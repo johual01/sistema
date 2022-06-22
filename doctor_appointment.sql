f6d6f666ad5da63c13b9c653e139c4320f20be4c
Johual01 <pereirajesusbenja@gmail.com>
2022-06-21 23:24:25 -0400

    actualizacionSQL

--- doctor_appointment.sql
+++ doctor_appointment.sql
@@ -43,7 +43,7 @@ CREATE TABLE `admin_table` (
 --
 
 INSERT INTO `admin_table` (`admin_id`, `admin_email_address`, `admin_password`, `admin_name`, `hospital_name`, `hospital_address`, `hospital_contact_no`, `hospital_logo`) VALUES
-(1, 'johnsmith@gmail.com', 'password', 'John smith', 'Mount Hospital', '115, Last Lane, NYC', '741287410', '../images/15001.png');
+(1, 'pereirajesusbenja@gmail.com', '0981284466', 'Jesús Pereira', 'Hospital Pereira', 'El lago Municipal 343', '021284466', '../images/15001.png');
 
 -- --------------------------------------------------------
 
@@ -69,13 +69,13 @@ CREATE TABLE `appointment_table` (
 --
 
 INSERT INTO `appointment_table` (`appointment_id`, `doctor_id`, `patient_id`, `doctor_schedule_id`, `appointment_number`, `reason_for_appointment`, `appointment_time`, `status`, `patient_come_into_hospital`, `doctor_comment`) VALUES
-(3, 1, 3, 2, 1000, 'Pain in Stomach', '09:00:00', 'Cancel', 'No', ''),
-(4, 1, 3, 2, 1001, 'Paint in stomach', '09:00:00', 'Booked', 'No', ''),
-(5, 1, 4, 2, 1002, 'For Delivery', '09:30:00', 'Completed', 'Yes', 'She gave birth to boy baby.'),
-(6, 5, 3, 7, 1003, 'Fever and cold.', '18:00:00', 'In Process', 'Yes', ''),
-(7, 6, 5, 13, 1004, 'Pain in Stomach.', '15:30:00', 'Completed', 'Yes', 'Acidity Problem. '),
-(8, 1, 6, 2, 1005, 'Si', '09:45:00', 'Booked', 'No', ''),
-(9, 2, 6, 3, 1006, 'Me quiero ir', '09:00:00', 'Booked', 'No', '');
+(3, 1, 3, 2, 1000, 'Dolor de estomago', '09:00:00', 'Cancelado', 'No', ''),
+(4, 1, 3, 2, 1001, 'Dolor de estomago', '09:00:00', 'Reservado', 'No', ''),
+(5, 1, 4, 2, 1002, 'Delivery frío', '09:30:00', 'Completado', 'Si', 'Su "delivery" era un hijo'),
+(6, 5, 3, 7, 1003, 'Fiebre y frío', '18:00:00', 'En proceso', 'Si', ''),
+(7, 6, 5, 13, 1004, 'Dolor de estomago', '15:30:00', 'Completado', 'Si', 'Demasiada acidez.'),
+(8, 1, 6, 2, 1005, 'Si', '09:45:00', 'Reservado', 'Si', ''),
+(9, 2, 6, 3, 1006, 'Me quiero ir', '09:00:00', 'Reservado', 'No', '');
 
 -- --------------------------------------------------------
 
@@ -99,19 +99,19 @@ CREATE TABLE `doctor_schedule_table` (
 --
 
 INSERT INTO `doctor_schedule_table` (`doctor_schedule_id`, `doctor_id`, `doctor_schedule_date`, `doctor_schedule_day`, `doctor_schedule_start_time`, `doctor_schedule_end_time`, `average_consulting_time`, `doctor_schedule_status`) VALUES
-(2, 1, '2022-06-19', 'Friday', '09:00', '14:00', 15, 'Active'),
-(3, 2, '2022-06-19', 'Friday', '09:00', '12:00', 15, 'Active'),
-(4, 5, '2022-06-19', 'Friday', '10:00', '14:00', 10, 'Active'),
-(5, 3, '2022-06-19', 'Friday', '13:00', '17:00', 20, 'Active'),
-(6, 4, '2022-06-19', 'Friday', '15:00', '18:00', 5, 'Active'),
-(7, 5, '2026-06-22', 'Monday', '18:00', '20:00', 10, 'Active'),
-(8, 2, '2022-06-24', 'Wednesday', '09:30', '12:30', 10, 'Active'),
-(9, 5, '2022-06-24', 'Wednesday', '11:00', '15:00', 10, 'Active'),
-(10, 1, '2022-06-24', 'Wednesday', '12:00', '15:00', 10, 'Active'),
-(11, 3, '2022-06-24', 'Wednesday', '14:00', '17:00', 15, 'Active'),
-(12, 4, '2022-06-24', 'Wednesday', '16:00', '20:00', 10, 'Active'),
-(13, 6, '2022-06-24', 'Wednesday', '15:30', '18:30', 10, 'Active'),
-(14, 6, '2022-06-25', 'Thursday', '10:00', '13:30', 10, 'Active');
+(2, 1, '2022-06-25', 'Viernes', '09:00', '14:00', 15, 'Activo'),
+(3, 2, '2022-06-25', 'Viernes', '09:00', '12:00', 15, 'Activo'),
+(4, 5, '2022-06-25', 'Viernes', '10:00', '14:00', 10, 'Activo'),
+(5, 3, '2022-06-25', 'Viernes', '13:00', '17:00', 20, 'Activo'),
+(6, 4, '2022-06-25', 'Viernes', '15:00', '18:00', 5, 'Activo'),
+(7, 5, '2026-06-27', 'Lunes', '18:00', '20:00', 10, 'Activo'),
+(8, 2, '2022-06-29', 'Miércoles', '09:30', '12:30', 10, 'Activo'),
+(9, 5, '2022-06-29', 'Miércoles', '11:00', '15:00', 10, 'Activo'),
+(10, 1, '2022-06-29', 'Miércoles', '12:00', '15:00', 10, 'Activo'),
+(11, 3, '2022-06-29', 'Miércoles', '14:00', '17:00', 15, 'Activo'),
+(12, 4, '2022-06-29', 'Miércoles', '16:00', '20:00', 10, 'Activo'),
+(13, 6, '2022-06-29', 'Miércoles', '15:30', '18:30', 10, 'Activo'),
+(14, 6, '2022-06-30', 'Jueves', '10:00', '13:30', 10, 'Activo');
 
 -- --------------------------------------------------------
 
@@ -139,12 +139,12 @@ CREATE TABLE `doctor_table` (
 --
 
 INSERT INTO `doctor_table` (`doctor_id`, `doctor_email_address`, `doctor_password`, `doctor_name`, `doctor_profile_image`, `doctor_phone_no`, `doctor_address`, `doctor_date_of_birth`, `doctor_degree`, `doctor_expert_in`, `doctor_status`, `doctor_added_on`) VALUES
-(1, 'peterparker@gmail.com', 'password', 'Dr. Peter Parker', '../images/10872.jpg', '7539518520', '102, Sky View, NYC', '1985-10-29', 'MBBS MS', 'Sergen', 'Active', '2021-02-15 17:04:59'),
-(2, 'adambrodly@gmail.com', 'password', 'Dr. Adam Broudly', '../images/21336.jpg', '753852963', '105, Fort, NYC', '1982-08-10', 'MBBS MD(Cardiac)', 'Cardiologist', 'Active', '2021-02-18 15:00:32'),
-(3, 'sophia.parker@gmail.com', 'password', 'Dr. Sophia Parker', '../images/13838.jpg', '7417417410', '50, Best street CA', '1989-04-03', 'MBBS', 'Gynacologist', 'Active', '2021-02-18 15:05:02'),
-(4, 'williampeterson@gmail.com', 'password', 'Dr. William Peterson', '../images/9498.jpg', '8523698520', '32, Green City, NYC', '1984-06-11', 'MBBS MD', 'Nurologist', 'Active', '2021-02-18 15:08:24'),
-(5, 'emmalarsdattor@gmail.com', 'password', 'Dr. Emma Larsdattor', '../images/1613641523.png', '9635852025', '25, Silver Arch', '1988-03-03', 'MBBS MD', 'General Physian', 'Active', '2021-02-18 15:15:23'),
-(6, 'manuel.armstrong@gmail.com', 'password', 'Dr. Manuel Armstrong', '../images/1614081376.png', '8523697410', '2378 Fire Access Road Asheboro, NC 27203', '1989-03-01', 'MBBS MD (Medicine)', 'General Physician', 'Active', '2021-02-23 17:26:16');
+(1, 'nosoyspiderman@gmail.com', 'password', 'Dr. Peter Parker', '../images/10872.jpg', '7539518520', '102, Sky View, NYC', '1985-10-29', 'Médico Cirujano', 'Cirugía', 'Activo', '2021-02-15 17:04:59'),
+(2, 'nosoysethcohen@gmail.com', 'password', 'Dr. Adam Broudly', '../images/21336.jpg', '753852963', '105, Fort, NYC', '1982-08-10', 'Médico Cirujano', 'Cardiología', 'Activo', '2021-02-18 15:00:32'),
+(3, 'nosoylamedium@gmail.com', 'password', 'Dr. Sophia Parker', '../images/13838.jpg', '7417417410', '50, Best street CA', '1989-04-03', 'Doctor Médico', 'Ginecología', 'Activo', '2021-02-18 15:05:02'),
+(4, 'nosoygilgrissom@gmail.com', 'password', 'Dr. William Petersen', '../images/9498.jpg', '8523698520', '32, Green City, NYC', '1984-06-11', 'PhD en Medicina', 'Neurología', 'Activo', '2021-02-18 15:08:24'),
+(5, 'nosoyelmonstruo@gmail.com', 'password', 'Dr. Emma Larsdattor', '../images/1613641523.png', '9635852025', '25, Silver Arch', '1988-03-03', 'Licenciatura en Psicología', 'Psicología', 'Activo', '2021-02-18 15:15:23'),
+(6, 'nopiselaluna@gmail.com', 'password', 'Dr. Manuel Armstrong', '../images/1614081376.png', '8523697410', '2378 Fire Access Road Asheboro, NC 27203', '1989-03-01', 'Médico Cirujano', 'Medicina General', 'Activo', '2021-02-23 17:26:16');
 
 -- --------------------------------------------------------
 
@@ -173,10 +173,10 @@ CREATE TABLE `patient_table` (
 --
 
 INSERT INTO `patient_table` (`patient_id`, `patient_email_address`, `patient_password`, `patient_first_name`, `patient_last_name`, `patient_date_of_birth`, `patient_gender`, `patient_address`, `patient_phone_no`, `patient_maritial_status`, `patient_added_on`, `patient_verification_code`, `email_verify`) VALUES
-(3, 'jacobmartin@gmail.com', 'password', 'Jacob', 'Martin', '2021-02-26', 'Male', 'Green view, 1025, NYC', '85745635210', 'Single', '2021-02-18 16:34:55', 'b1f3f8409f7687072adb1f1b7c22d4b0', 'Yes'),
-(4, 'oliviabaker@gmail.com', 'password', 'Olivia', 'Baker', '2001-04-05', 'Female', 'Diamond street, 115, NYC', '7539518520', 'Married', '2021-02-19 18:28:23', '8902e16ef62a556a8e271c9930068fea', 'Yes'),
-(5, 'web-tutorial@programmer.net', 'password', 'Amber', 'Anderson', '1995-07-25', 'Female', '2083 Cameron Road Buffalo, NY 14202', '75394511442', 'Single', '2021-02-23 17:50:06', '1909d59e254ab7e433d92f014d82ba3d', 'Yes'),
-(6, 'pereirajesusbenja@gmail.com', '0981284466', 'Jesus', 'Pereira', '2022-06-07', 'Male', 'William Richardson\r\nAraucanos', '5919191919', 'Single', '2022-06-12 22:33:48', 'bca1b051aa5705d07bea4aae099f724f', 'Yes');
+(3, 'JacoboMartinez@gmail.com', 'password', 'Jacobo', 'Martinez', '2021-02-26', 'Male', 'Vista verde 116', '85745635210', 'Soltero', '2021-02-18 16:34:55', 'b1f3f8409f7687072adb1f1b7c22d4b0', 'Yes'),
+(4, 'oliviapastel@gmail.com', 'password', 'Olivia', 'Pastel', '2001-04-05', 'Female', 'La calle de los Diamantes 1237', '7539518520', 'Casado', '2021-02-19 18:28:23', '8902e16ef62a556a8e271c9930068fea', 'Yes'),
+(5, 'amberheart@gmail.com', 'password', 'Amber', 'Hearth', '1995-07-25', 'Female', 'El camino del  Yacaré 123', '75394511442', 'Soltero', '2021-02-23 17:50:06', '1909d59e254ab7e433d92f014d82ba3d', 'Yes'),
+(6, 'pereirajesusbenja@gmail.com', '0981284466', 'Jesus', 'Pereira', '2022-06-07', 'Male', 'William Richardson\r\nAraucanos', '5919191919', 'Soltero', '2022-06-12 22:33:48', 'bca1b051aa5705d07bea4aae099f724f', 'Yes');
 
 --
 -- Índices para tablas volcadas
